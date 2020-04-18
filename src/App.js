9461d7110e8411b5ae78b3324c6c57deb303702c
jackdo69 <jackie.duc.do@gmail.com>
2020-04-18 14:05:19 +0700

    load the user location

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
+import L from "leaflet";
+import {
+  Map,
+  TileLayer,
+  Marker,
+  Popup,
+} from "react-leaflet";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var icon = L.icon({
+  iconUrl:
+    "https://unpkg.com/leaflet@1.6.0/dist/images/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [22, 94],
+  popupAnchor: [0, -41],
+});
+class App extends Component {
+  state = {
+    location: {
+      lat: 51.505,
+      lng: -0.09,
+    },
+    haveUsersLocation: false,
+    zoom: 2,
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          location: {
+            lat:
+              position.coords.latitude,
+            lng:
+              position.coords.longitude,
+          },
+          haveUsersLocation: true,
+          zoom: 13,
+        });
+      }
+    );
+  }
+
+  render() {
+    const position = [
+      this.state.location.lat,
+      this.state.location.lng,
+    ];
+    return (
+      <Map
+        className="map"
+        center={position}
+        zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {this.state
+          .haveUsersLocation ? (
+          <Marker
+            icon={icon}
+            position={position}>
+            <Popup>
+              A pretty CSS3 popup.{" "}
+              <br /> Easily
+              customizable.
+            </Popup>
+          </Marker>
+        ) : (
+          ""
+        )}
+      </Map>
+    );
+  }
 }
 
 export default App;
